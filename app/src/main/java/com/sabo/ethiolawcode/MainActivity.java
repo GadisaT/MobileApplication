b68724af2b9f1cb1b3af30eddb36f0981a5ba1fa
sabokadiriba <sabokadiriba02@gmail.com>
2022-12-27 19:48:21 +0300

    option added

--- app/src/main/java/com/sabo/ethiolawcode/MainActivity.java
+++ app/src/main/java/com/sabo/ethiolawcode/MainActivity.java
@@ -1,8 +1,12 @@
 package com.sabo.ethiolawcode;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.optionmenu,menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case  R.id.itemsetting:
+                Toast.makeText(this, "setting item selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case  R.id.itemother:
+                Toast.makeText(this, "other option item selected", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 }
